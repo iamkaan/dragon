bfe4c081f37fbab87f34ab3e322ef61260749bee
iamkaan <kaanmamikoglu@gmail.com>
2018-10-06 13:53:35 +0200

    todos

--- app/src/main/java/com/iamkaan/dragon/DataManager.kt
+++ app/src/main/java/com/iamkaan/dragon/DataManager.kt
@@ -1,7 +1,6 @@
 package com.iamkaan.dragon
 
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
 import com.iamkaan.dragon.home.DragonViewModel
 import java.util.*
 
@@ -16,80 +15,23 @@ class DataManager(
     private val firestore = FirebaseFirestore.getInstance()
 
     fun getDragons(onComplete: (List<DragonViewModel>) -> Unit) {
-        firestore.collection("dragons")
-                .orderBy("time", Query.Direction.DESCENDING)
-                .addSnapshotListener { querySnapshot, exception ->
-                    if (exception != null) {
-                        logger.e("Could not get the list of dragons", exception)
-                    } else {
-                        val viewModels = dataMapper.mapData(querySnapshot!!, ::updateLikes, ::deleteDragon)
-                        onComplete(viewModels)
-                    }
-                }
+        //TODO("Implement the getDragons method")
     }
 
     fun getFilteredDragons(onComplete: (List<DragonViewModel>) -> Unit) {
-        firestore.collection("dragons")
-                .whereGreaterThanOrEqualTo("time", tenMinAgo())
-                .whereArrayContains("likes", userManager.getUserId()!!)
-                .orderBy("time", Query.Direction.DESCENDING)
-                .addSnapshotListener { querySnapshot, exception ->
-                    if (exception != null) {
-                        logger.e("Could not get the list of dragons", exception)
-                    } else {
-                        val viewModels = dataMapper.mapData(querySnapshot!!, ::updateLikes, ::deleteDragon)
-                        onComplete(viewModels)
-                    }
-                }
+        //TODO("Implement the getFilteredDragons method")
     }
 
     fun postDragon(dataModel: DragonDataModel, onComplete: (success: Boolean) -> Unit) {
-        firestore.collection("dragons")
-                .add(dataModel)
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        onComplete(true)
-                    } else {
-                        logger.e("Could not post the new dragon", it.exception)
-                        onComplete(false)
-                    }
-                }
+        //TODO("Implement the postDragon method")
     }
 
     private fun deleteDragon(dragonId: String, onComplete: (success: Boolean) -> Unit) {
-        firestore.collection("dragons")
-                .document(dragonId)
-                .delete()
-                .addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        onComplete(true)
-                    } else {
-                        logger.e("Could not delete the dragon", it.exception)
-                        onComplete(false)
-                    }
-                }
+        //TODO("Implement the deleteDragon method")
     }
 
     private fun updateLikes(dragonId: String, liked: Boolean, onComplete: (success: Boolean) -> Unit) {
-        firestore.runTransaction { transaction ->
-            val reference = firestore.collection("dragons").document(dragonId)
-            val snapshot = transaction.get(reference)
-            val likes = snapshot.get("likes") as ArrayList<String>
-            if (liked) {
-                likes.add(userManager.getUserId()!!)
-            } else {
-                likes.remove(userManager.getUserId()!!)
-            }
-            transaction.update(reference, "likes", likes)
-            transaction.update(reference, "likeCount", likes.size)
-        }.addOnCompleteListener {
-            if (it.isSuccessful) {
-                onComplete(true)
-            } else {
-                logger.e("Could not update the likes for the dragon", it.exception)
-                onComplete(false)
-            }
-        }
+        //TODO("Implement the updateLikes method")
     }
 
     private fun tenMinAgo() = Date().apply {
