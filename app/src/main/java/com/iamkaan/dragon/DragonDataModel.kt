97b86cc85ad08b0c2b3d27f0454e270c5653d27b
iamkaan <kaanmamikoglu@gmail.com>
2018-10-26 16:57:13 +0100

    tidy up and add missing dependency
    (cherry picked from commit 4c62425aea2b2a294ca0a67d7a6d82d63579dd3f)

--- app/src/main/java/com/iamkaan/dragon/DragonDataModel.kt
+++ app/src/main/java/com/iamkaan/dragon/DragonDataModel.kt
@@ -2,6 +2,12 @@ package com.iamkaan.dragon
 
 import java.util.*
 
-data class DragonDataModel(val name: String, val ownerId: String, val imageUrl: String, val time: Date, val likes: List<String>) {
+data class DragonDataModel(
+        val name: String,
+        val ownerId: String,
+        val imageUrl: String,
+        val time: Date,
+        val likes: List<String>
+) {
     constructor() : this("", "", "", Date(), emptyList())
 }
